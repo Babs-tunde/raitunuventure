9a116b9f9c6f2eee7a487ae202c492438d9f5cc4
Babs-tunde <smustaphababatunde@gmail.com>
2025-02-20 14:17:02 +0100

    some settings

--- wholesale_app/settings.py
+++ wholesale_app/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') 
 # Add this if you have a static directory
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
