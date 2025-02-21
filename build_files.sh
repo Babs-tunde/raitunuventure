155549dc64a90347bf92c744f3ff588677fa2db5
Babs-tunde <smustaphababatunde@gmail.com>
2025-02-21 10:17:39 +0100

    build scripts_ new

--- build_files.sh
+++ build_files.sh
@@ -2,8 +2,13 @@
 
 export PATH="$HOME/.local/bin:$PATH"
 
+# Activate the virtual environment
+source virt/bin/activate
+
+# Upgrade pip and install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Apply migrations and collect static files
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
