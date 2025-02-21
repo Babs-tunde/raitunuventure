bf9f2d29cf98badb914a9024ebcd943c983f521e
Babs-tunde <smustaphababatunde@gmail.com>
2025-02-21 09:37:06 +0100

    static files

--- wholesale_app/settings.py
+++ wholesale_app/settings.py
@@ -139,6 +139,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_URL = "img/"
+MEDIAL_ROOT = BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
