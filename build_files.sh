a55a16faea7451d6d6e5c31c976795d3071a888a
Babs-tunde <smustaphababatunde@gmail.com>
2025-02-21 10:10:53 +0100

    build scripts

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+export PATH="$HOME/.local/bin:$PATH"
+
+pip install --upgrade pip
 pip install -r requirements.txt
-python3.10 manage.py collectstatic
+
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
